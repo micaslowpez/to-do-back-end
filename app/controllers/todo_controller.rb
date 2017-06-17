c8aa0a1833cc6912976cd6000fa57f22175e3c93
America <americalopezsubero@gmail.com>
2017-06-17 21:38:53 +0000

    Linked index to show pages

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,8 +2,35 @@ class TodoController < ApplicationController
  
   def index
   end
-  
+
   def show
+  todo_id = params[:id]
   end
   
+ #The ... signifies that there is more code in this section, but is not shown in the example for the sake of brevity
+
+
+# def show
+#   todo_id = '1'
+
+#   if todo_id == '1' # Remember, the `==` conditional operator is used to check if two things are equal to each other.
+#     @todo_description = "Make the curriculum"
+   
+#   end
+# end
+
+
+
+def show
+  todo_id = '1'
+
+  if todo_id == '1'
+    @todo_description = "Wash dishes"
+
+
+  elsif todo_id == '2'
+    @todo_description = "Do laundry"
+    
+  end
+end
 end
